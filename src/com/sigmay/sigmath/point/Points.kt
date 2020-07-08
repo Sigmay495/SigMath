9069442e6cda8feaf3db35b5d92933bcafc531ba
Sigmay <sigmay.zen@gmail.com>
2020-07-08 21:06:37 +0900

    距離関数の追加

--- src/com/sigmay/sigmath/point/Points.kt
+++ src/com/sigmay/sigmath/point/Points.kt
@@ -1,5 +1,10 @@
 package com.sigmay.sigmath.point
 
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 /**
  * [x] と [y] を座標値に持つPointを生成する。
  */
@@ -39,3 +44,33 @@ fun mutablePoint3dOf(x: Double, y: Double, z: Double): MutablePoint3d = DoublePo
  * [x] と [y] と [z] を座標値に持つMutablePoint3dを生成する。
  */
 fun mutablePoint3dOf(x: Int, y: Int, z: Int): MutablePoint3d = IntPoint(x, y, z)
+
+/**
+ * 点 [p1] と点 [p2] 間の2次元ユークリッド距離を求める。
+ */
+fun distance(p1: Point, p2: Point) = p1.distance(p2)
+
+/**
+ * 点 [p1] と点 [p2] 間の3次元ユークリッド距離を求める。
+ */
+fun distance(p1: Point3d, p2: Point3d) = p1.distance(p2)
+
+/**
+ * 点 [p1] と点 [p2] 間の2次元マンハッタン距離を計算する。
+ */
+fun manhattanDistance(p1: Point, p2: Point)= p1.manhattanDistance(p2)
+
+/**
+ * 点 [p1] と点 [p2] 間の3次元マンハッタン距離を計算する。
+ */
+fun manhattanDistance(p1: Point3d, p2: Point3d)= p1.manhattanDistance(p2)
+
+/**
+ * 点 [p1] と点 [p2] 間の2次元チェビシェフ距離を計算する。
+ */
+fun chebyshevDistance(p1: Point, p2: Point)= p1.chebyshevDistance(p2)
+
+/**
+ * 点 [p1] と点 [p2] 間の3次元チェビシェフ距離を計算する。
+ */
+fun chebyshevDistance(p1: Point3d, p2: Point3d)= p1.chebyshevDistance(p2)
--- src/com/sigmay/sigmath/vector/Vectors.kt
+++ src/com/sigmay/sigmath/vector/Vectors.kt
@@ -39,3 +39,23 @@ fun mutableVectorOf(dim: Int): MutableVector = DoubleVector(dim)
  * 関数 [init] で初期化した要素を持つ [dim] 次元のMutableVectorを生成する。
  */
 fun mutableVectorOf(dim: Int, init: (Int) -> Double): MutableVector = DoubleVector(dim, init)
+
+/**
+ * ベクトル [v1] と [v2] 間のユークリッド距離を計算する。
+ */
+fun distance(v1: Vector, v2: Vector) = v1.distance(v2)
+
+/**
+ * ベクトル [v1] と [v2] 間のマンハッタン距離を計算する。
+ */
+fun manhattanDistance(v1: Vector, v2: Vector) = v1.manhattanDistance(v2)
+
+/**
+ * ベクトル [v1] と [v2] 間のチェビシェフ距離を計算する。
+ */
+fun chebyshevDistance(v1: Vector, v2: Vector) = v1.chebyshevDistance(v2)
+
+/**
+ * ベクトル [v1] と [v2] 間のコサイン類似度を計算する。
+ */
+fun cos(v1: Vector, v2: Vector) = v1.cos(v2)
